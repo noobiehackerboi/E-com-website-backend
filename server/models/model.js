6ae32dbcd2b20d2c49a25effa7f72377aca12614
Divya <divyaratna1234@gmail.com>
2022-08-19 04:23:02 +0530

    Build

--- server/models/model.js
+++ server/models/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const userAdminSchema = new mongoose.Schema({
-    admin_id: {
+const userSchema = new mongoose.Schema({
+    user_id: {
         type: String,
         required: [true, "can't be blank"],
         unique: true
@@ -10,64 +10,6 @@ const userAdminSchema = new mongoose.Schema({
         required: [true, "can't be blank"],
     }
 })
-const userStaffSchema = new mongoose.Schema({
-    staff_id: {
-        // staff_id will be of format :
-        // "SCH/CL/xxxx" (CL represents cleaning staff)
-        // "SCH/TH/xxxx" (TH represents teaching staff)
-        type: String,
-        required: [true, "can't be blank"],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, "can't be blank"]
-    },
-    email: {
-        type: String,
-        required: [true, "can't be blank"],
-        unique: true
-    }
-})
-const userStudentSchema = new mongoose.Schema({
-    student_id: {
-        // student_id will be of format
-        // 2022 represents admission year
-        // "2022/xxxx"
-        type: String,
-        required: [true, "can't be blank"],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, "can't be blank"]
-    },
-    email: {
-        type: String,
-        required: [true, "can't be blank"],
-        unique: true
-    }
-})
-const noticeSchema = new mongoose.Schema({
-    heading: {
-        type: String,
-        required: [true, "can't be blank"],
-    },
-    body: {
-        type: String,
-        required: [true, "can't be blank"]
-    },
-    date: {
-        type: Date,
-        required: [true, "can't be blank"]
-    }
-})
-const userAdmin = mongoose.model('admin', userAdminSchema, 'admin');
-const userStaff = mongoose.model('staff', userStaffSchema, 'staffs');
-const userStudent = mongoose.model('student', userStudentSchema, 'students');
-const userNotices = mongoose.model('notice', noticeSchema, 'notices')
-
+const user = mongoose.model('user', userAdminSchema, 'users');
 // Exporting our model objects
-module.exports = {
-    userAdmin, userStaff, userStudent, userNotices
-}
+module.exports = userSchema
--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const bodyParser = require('body-parser')
 const api = require('./routes/api')
 const cors = require('cors')
 
-const PORT = process.env.PORT || 8080
+// const PORT = process.env.PORT || 8080
+const PORT = 3000
 
 const app = express()
 app.use(cors())
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-})
+// app.use(express.static('public'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// })
 
 app.use(bodyParser.json());
 
